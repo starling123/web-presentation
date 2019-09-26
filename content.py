0563bad1a8c94029501ed1192ea0b59ede6461b5
starling123 <37346749+starling123@users.noreply.github.com>
2019-09-26 21:23:42 +0200

    Add files via upload
    initial upload

---
+++ content.py
@@ -0,0 +1,7 @@
+content = [
+    {"title":"Eerste sheet"}
+    ,{"title":"Tweede sheet"}
+    ,{"title":"Nu volgt een liedje"}
+    ,{"title":"Derde sheet", "template":"plaatje.html","plaatje":"liedje.png"}
+    ,{"title":"vierde sheet"}
+]    
